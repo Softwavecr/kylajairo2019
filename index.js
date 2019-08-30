78e01872b08f5e00f8aacac532e70e30edf1e743
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 05:09:41 -0400

    pug works online now, adding ejs

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));//router.set('port', PORT);
 
-//app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('view engine', 'pug');
 
 
-//app.get('/', function(req, res) {  res.render('pages/index.ejs', {     title: 'Home KJ2019'  });  });
+app.get('/', function(req, res) {  res.render('pages/index.ejs', {     title: 'Home KJ2019'  });  });
 app.get('/index1', function(req, res) {  res.render('index1.pug', {     title: 'iHome KJ2019'  });  });
 
 
