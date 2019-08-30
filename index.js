7f7f32ad51eee95dce9d9e8bf444a7f9365dee5e
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 04:02:51 -0400

    ++ wedding, shouldnt open, b index p should b ok

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const PORT = process.env.PORT || 5000
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));//router.set('port', PORT);
-app.set('view engine', 'ejs');
-app.get('/', function(req, res) {  res.render('pages/index', {     title: 'Home KJ2019'  });  });
 
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req, res) {  res.render('pages/index', {     title: 'Home KJ2019'  });  });
 // app.get('/index1', function(req, res) {  res.render('index1', {     title: 'iHome KJ2019'  });  });
-// app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}))
+
+
+app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}));
 // app.get('/travel', (req, res) => res.render('travel', {title: 'tHome KJ2019'}))
 // app.get('/registry', (req, res) => res.render('registry', {title: 'rHome KJ2019'}))
 // app.get('/venue', (req, res) => res.render('venue', {title: 'vHome KJ2019'}))
