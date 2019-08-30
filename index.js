f9179fa6214bd905133339936deb80e3fe336052
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 04:22:39 -0400

    support to multiple engines ejs + pug

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));//router.set('port', PORT);
 
 app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 
-app.get('/', function(req, res) {  res.render('pages/index', {     title: 'Home KJ2019'  });  });
+app.get('/', function(req, res) {  res.render('pages/index.ejs', {     title: 'Home KJ2019'  });  });
 // app.get('/index1', function(req, res) {  res.render('index1', {     title: 'iHome KJ2019'  });  });
 
 
-app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}));
+app.get('/wedding', (req, res) => res.render('wedding.pug', {title: 'wHome KJ2019'}));
 // app.get('/travel', (req, res) => res.render('travel', {title: 'tHome KJ2019'}))
 // app.get('/registry', (req, res) => res.render('registry', {title: 'rHome KJ2019'}))
 // app.get('/venue', (req, res) => res.render('venue', {title: 'vHome KJ2019'}))
