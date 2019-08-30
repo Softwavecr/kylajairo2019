44ed5eeae797d67692e2292b142f18bce353e34f
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 03:37:28 -0400

    troubleshooting

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'));//router.set('port', PORT);
 app.get('/', function(req, res) {  res.render('index', {     title: 'Home KJ2019'  });  });
+
 app.get('/index1', function(req, res) {  res.render('index1', {     title: 'iHome KJ2019'  });  });
 app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}))
 app.get('/travel', (req, res) => res.render('travel', {title: 'tHome KJ2019'}))
