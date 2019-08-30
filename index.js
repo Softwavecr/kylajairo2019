d26c9eb60811069440a583f61fa56f9a3ccdd518
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 03:43:22 -0400

    ts deleting files

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'));//router.set('port', PORT);
 app.get('/', function(req, res) {  res.render('index', {     title: 'Home KJ2019'  });  });
 
-app.get('/index1', function(req, res) {  res.render('index1', {     title: 'iHome KJ2019'  });  });
-app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}))
-app.get('/travel', (req, res) => res.render('travel', {title: 'tHome KJ2019'}))
-app.get('/registry', (req, res) => res.render('registry', {title: 'rHome KJ2019'}))
-app.get('/venue', (req, res) => res.render('venue', {title: 'vHome KJ2019'}))
-app.get('/todo', (req, res) => res.render('todo', {title: 'toHome KJ2019'}))
-app.get('/food', (req, res) => res.render('food', {title: 'fHome KJ2019'}))
+// app.get('/index1', function(req, res) {  res.render('index1', {     title: 'iHome KJ2019'  });  });
+// app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}))
+// app.get('/travel', (req, res) => res.render('travel', {title: 'tHome KJ2019'}))
+// app.get('/registry', (req, res) => res.render('registry', {title: 'rHome KJ2019'}))
+// app.get('/venue', (req, res) => res.render('venue', {title: 'vHome KJ2019'}))
+// app.get('/todo', (req, res) => res.render('todo', {title: 'toHome KJ2019'}))
+// app.get('/food', (req, res) => res.render('food', {title: 'fHome KJ2019'}))
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
