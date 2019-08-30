f86bf39276e64321500c640264c4d5eeab61647c
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 05:06:20 -0400

    ++pug package and it included many other dep

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path')
-const pug = require('pug')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const pug = require('pug');
+const PORT = process.env.PORT || 5000;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
