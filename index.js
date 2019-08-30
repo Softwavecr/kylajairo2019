8fadfbefaf0f73ff80809fadef5bd44b93cd1815
Softwavecr <mdai101@live.rhul.ac.uk>
2019-08-30 03:05:18 -0400

    ready? all pages, images, running as expected locally, first attempt to publish

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));//router.set('port', PORT);
+app.get('/', function(req, res) {  res.render('index', {     title: 'Home KJ2019'  });  });
+app.get('/wedding', (req, res) => res.render('wedding', {title: 'wHome KJ2019'}))
+app.get('/travel', (req, res) => res.render('travel', {title: 'tHome KJ2019'}))
+app.get('/registry', (req, res) => res.render('registry', {title: 'rHome KJ2019'}))
+app.get('/venue', (req, res) => res.render('venue', {title: 'vHome KJ2019'}))
+app.get('/todo', (req, res) => res.render('todo', {title: 'toHome KJ2019'}))
+app.get('/food', (req, res) => res.render('food', {title: 'fHome KJ2019'}))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
